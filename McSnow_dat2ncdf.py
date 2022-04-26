66bcca9c26522d1a3f3d645d240ef680713743ae
Leonie von Terzi <lterzi@uni-koeln.de>
2022-04-26 10:44:28 +0200

    changed header and table layout of McSnow output

--- McSnow_dat2ncdf.py
+++ McSnow_dat2ncdf.py
@@ -10,11 +10,15 @@ import os
 experimentID = os.environ['experiment']
 inputPath = os.environ['MCexp']+'experiments/'+experimentID+'/'
 
-data = pd.read_csv(inputPath+'mass2fr.dat',header=0)
+#inputPath = '/work/lvonterz/McSnow_habit/experiments/Jan_Niklas_frag_Leonie_setup/'
+
+data = pd.read_csv(inputPath+'mass2fr.dat',header=0)#,skiprows=1, # header=0
+                   #names=['time','mTot','sHeight','vel','dia','area','sMice','sVice','sPhi','sRhoIce','sNmono','sMrime','sVrime','sMult', 'sRho_tot'])
 data.columns = data.columns.str.replace(' ','')
+
 # converting pandas dataframe into xarray to add attrs and save into netcdf
 dataXR = data.to_xarray()
-dataXR['time'].attrs={'units':'s','long_name':'seconds since start of simulation'}
+dataXR['time'].attrs={'units':'min','long_name':'minutes since start of simulation'}
 dataXR['mTot'].attrs={'units':'kg','long_name':'total mass of superparticle (ice+rime+liquid)'}
 dataXR['mTot'].attrs={'units':'kg','long_name':'total mass of superparticle (ice+rime+liquid)'}
 dataXR['sHeight'].attrs={'units':'m','long_name':'height of superparticle'}
@@ -25,14 +29,19 @@ dataXR['sMice'].attrs={'units':'kg','long_name':'ice mass of superparticle'}
 dataXR['sVice'].attrs={'units':'m^3','long_name':'ice volume of superparticle'}
 dataXR['sPhi'].attrs={'units':'','long_name':'aspect ratio of superparticle'}
 dataXR['sRhoIce'].attrs={'units':'kg/m^3','long_name':'density of ice of superparticle'}
-dataXR['igf'].attrs={'units':'','long_name':'inherent growth function'}
+#dataXR['igf'].attrs={'units':'','long_name':'inherent growth function'}
 dataXR['sNmono'].attrs={'units':'','long_name':'number of monomers of superparticle'}
 dataXR['sMrime'].attrs={'units':'kg','long_name':'rime mass of superparticle'}
 dataXR['sVrime'].attrs={'units':'m^3','long_name':'rime volume of superparticle'}
 dataXR['sMult'].attrs={'units':'','long_name':'multiplicity of superparticle'}
+#dataXR['sMliqu'].attrs={'units':'kg','long_name':'liquid water mass of superparticle'}
+#dataXR['sMmelt'].attrs={'units':'kg','long_name':'melted or frozen water mass of superparticle'}
+dataXR['sRho_tot'].attrs={'units':'kg/m^3','long_name':'total density of superparticle'}
+dataXR = dataXR.fillna(0) 
 
 #save as nc
-dataXR.to_netcdf(inputPath+'mass2fr.nc')
 
+dataXR.to_netcdf(inputPath+'mass2fr.nc')
+print('saved at ', inputPath+'mass2fr.nc')
 #remove .dat file
 os.remove(inputPath+'mass2fr.dat')
