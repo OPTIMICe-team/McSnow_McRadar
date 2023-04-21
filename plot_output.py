1b4bb464f4363344ac8a4a488bdef5395e586fa5
Leonie von Terzi <L.Terzi@lmu.de>
2023-04-21 11:20:36 +0200

    don't compute properties plot if already there because it takes a long time

--- plot_output.py
+++ plot_output.py
@@ -27,14 +27,17 @@ print(inputPath)
 #convolute=os.environ['convolute']
 print('loading the settings')
 splitPath = inputPath.split('domtop')[1]
+print(splitPath)
 domTop = splitPath[0:4]
 lutPath = os.environ['LUT_dir']
-
+box_area = inputPath.split('ba')[1].split('_')[0]
+box_area=float(box_area)/100
 # decide what you want to plot
 plot_initemp = False
 plot_inidia = False
 plot_thesis_particle_evolution = False
 mult_conc = False
+#plotProp = False
 single_particle=str2bool(os.environ['singleParticle'])
 convoluted=str2bool(os.environ['convolute'])
 
@@ -45,7 +48,7 @@ if ('trajectories' not in experimentID) and ('trajectories' not in inputPath):
 else:
 	heightRes = 2
 dicSettings = mcr.loadSettings(dataPath=inputPath+'mass2fr.nc',
-                               elv=elv, freq=freq,gridBaseArea=5.0,maxHeight=int(domTop),
+                               elv=elv, freq=freq,gridBaseArea=box_area,maxHeight=int(domTop),
                                ndgsVal=50,heightRes=heightRes,scatSet={'mode':scatMode, 'lutPath':lutPath,'particle_name':particle_name,'safeTmatrix':False})
 
 print('loading the McSnow output')
@@ -90,47 +93,48 @@ if ('trajectories' not in experimentID) and ('trajectories' not in inputPath):
 	velBins = np.linspace(-3,0,100)
 	dBins = 10**(np.linspace(-3,0,100))
 	#print(dBins)
-	
-	print('plotting particle properties')
-	fig,ax=plt.subplots(ncols=3,nrows=2,figsize=(15,10))
-	varVec = ['dia','mTot','sRho_tot']
-	for i,var in enumerate(varVec):	
-		print(var)
-		ax[0,i]=plot.plotPropSpecThesis(ax[0,i],dicSettings['heightRange'],dicSettings['heightRes'],mcTableTmp,velBins,var)
-		ax[0,i].set_ylim([mcTableTmp['Temp'].max()+0.1,mcTableTmp['Temp'].min()-0.1])
-		ax[0,i].set_xlim([-2,0])
-		ax[0,i].tick_params(axis='both',labelsize=16)
-		ax[0,i].text(ax[0,i].get_xlim()[0]+0.04*(ax[0,i].get_xlim()[1]-ax[0,i].get_xlim()[0]),-27,'('+string.ascii_lowercase[i]+')',fontsize=18)
-		ax[0,i].grid(ls='-.')
-		if mcTableTmp['Temp'].min() < -20:
-			ax[0,i].axhline(y=-20,ls='--',color='r',linewidth=2)
-			ax[0,i].axhline(y=-10,ls='--',color='r',linewidth=2)
-		if i == 0:
-			ax[0,i].set_ylabel('T [°C]',fontsize=18)
-		else:
-			ax[0,i].set_ylabel('')
-			ax[0,i].set_yticklabels('')
-	#plot.plotPropSpecThesis(dicSettings,mcTable,velBins,inputPath,'dia_cm')
-	varVec = ['sNmono','sPhi','number_conc']
-	for i,var in enumerate(varVec):	
-		print(var)
-		ax[1,i]=plot.plotPropSpecThesis(ax[1,i],dicSettings['heightRange'],dicSettings['heightRes'],mcTableTmp,velBins,var)
-		ax[1,i].set_xlim([-2,0])
-		ax[1,i].set_ylim([mcTableTmp['Temp'].max()+.1,mcTableTmp['Temp'].min()-.1])
-		ax[1,i].tick_params(axis='both',labelsize=16)
-		ax[1,i].text(ax[1,i].get_xlim()[0]+0.04*(ax[1,i].get_xlim()[1]-ax[1,i].get_xlim()[0]),-27,'('+string.ascii_lowercase[i+3]+')',fontsize=18)
-		ax[1,i].grid(ls='-.')
-		if mcTableTmp['Temp'].min() < -20:
-			ax[1,i].axhline(y=-20,ls='--',color='r',linewidth=2)
-			ax[1,i].axhline(y=-10,ls='--',color='r',linewidth=2)
-		if i == 0:
-			ax[1,i].set_ylabel('T [°C]',fontsize=18)
-		else:
-			ax[1,i].set_ylabel('')
-			ax[1,i].set_yticklabels('')
-	plt.tight_layout()
-	plt.savefig(inputPath+'properties.png')
-	plt.close()
+	#if plotProp:
+	if not os.path.isfile(inputPath+'properties.png'):
+		print('plotting particle properties')
+		fig,ax=plt.subplots(ncols=3,nrows=2,figsize=(15,10))
+		varVec = ['dia','mTot','sRho_tot']
+		for i,var in enumerate(varVec):	
+			print(var)
+			ax[0,i]=plot.plotPropSpecThesis(ax[0,i],dicSettings['heightRange'],dicSettings['heightRes'],mcTableTmp,velBins,var)
+			ax[0,i].set_ylim([mcTableTmp['Temp'].max()+0.1,mcTableTmp['Temp'].min()-0.1])
+			ax[0,i].set_xlim([-2,0])
+			ax[0,i].tick_params(axis='both',labelsize=16)
+			#ax[0,i].text(ax[0,i].get_xlim()[0]+0.04*(ax[0,i].get_xlim()[1]-ax[0,i].get_xlim()[0]),-27,'('+string.ascii_lowercase[i]+')',fontsize=18)
+			ax[0,i].grid(ls='-.')
+			if mcTableTmp['Temp'].min() < -20:
+				ax[0,i].axhline(y=-20,ls='--',color='r',linewidth=2)
+				ax[0,i].axhline(y=-10,ls='--',color='r',linewidth=2)
+			if i == 0:
+				ax[0,i].set_ylabel('T [°C]',fontsize=18)
+			else:
+				ax[0,i].set_ylabel('')
+				ax[0,i].set_yticklabels('')
+		#plot.plotPropSpecThesis(dicSettings,mcTable,velBins,inputPath,'dia_cm')
+		varVec = ['sNmono','sPhi','number_conc']
+		for i,var in enumerate(varVec):	
+			print(var)
+			ax[1,i]=plot.plotPropSpecThesis(ax[1,i],dicSettings['heightRange'],dicSettings['heightRes'],mcTableTmp,velBins,var)
+			ax[1,i].set_xlim([-2,0])
+			ax[1,i].set_ylim([mcTableTmp['Temp'].max()+.1,mcTableTmp['Temp'].min()-.1])
+			ax[1,i].tick_params(axis='both',labelsize=16)
+			#ax[1,i].text(ax[1,i].get_xlim()[0]+0.04*(ax[1,i].get_xlim()[1]-ax[1,i].get_xlim()[0]),-27,'('+string.ascii_lowercase[i+3]+')',fontsize=18)
+			ax[1,i].grid(ls='-.')
+			if mcTableTmp['Temp'].min() < -20:
+				ax[1,i].axhline(y=-20,ls='--',color='r',linewidth=2)
+				ax[1,i].axhline(y=-10,ls='--',color='r',linewidth=2)
+			if i == 0:
+				ax[1,i].set_ylabel('T [°C]',fontsize=18)
+			else:
+				ax[1,i].set_ylabel('')
+				ax[1,i].set_yticklabels('')
+		plt.tight_layout()
+		plt.savefig(inputPath+'properties.png')
+		plt.close()
 	#quit()
 	
 	#-- plot number of superparticles per grid cell
@@ -138,8 +142,10 @@ if ('trajectories' not in experimentID) and ('trajectories' not in inputPath):
 	nz = float(inputPath.split('nz')[1].split('_')[0])
 	#heightProf = pd.read_csv(inputPath+'hei2massdens.dat',header=0)#,skiprows=1, # header=0
 	#print(heightProf)
+	print('plotting number concentration profile')
+	plot.plotconcHeightProf('Real',mcTable,inputPath,dicSettings)
 	print('plotting number of sp')
-	plot.plotHeightProf(nz,mcTable,inputPath,dicSettings)
+	plot.plotconcHeightProf('Super',mcTable,inputPath,dicSettings,nz=nz)
 	print('plotting PSD')
 	mBins = 10**(np.linspace(-12,-9,100))
 	plot.plotPSD(mcTable,dicSettings,inputPath,mBins,'mTot',heightEdge0=1900,unit='[kg]',sepMono=True,yscale='log',xscale='log')
@@ -164,7 +170,7 @@ if ('trajectories' not in experimentID) and ('trajectories' not in inputPath):
 		if len(freq)==2:
 			plot.plotOverview(output,dicSettings,inputPath,dicSettings['wl'][0],dicSettings['wl'][1])
 		elif len(freq)==3:
-			plot.plotOverview(output,dicSettings,inputPath,dicSettings['wl'][1],dicSettings['wl'][2])
+			plot.plotOverview(output,dicSettings,inputPath,dicSettings['wl'][0],dicSettings['wl'][1],wl3=dicSettings['wl'][2])
 		print('plotting spectra')
 		if minmax:
 			plot.plotSpectra(dicSettings,output,inputPath,minmax=minmax,plotTemp=plotTemp)#,mult_conc=mult_conc)#,convoluted=True)
